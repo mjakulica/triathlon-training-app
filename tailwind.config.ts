3c557ff11017933f175466f5806574236d3e30b3
mjakulica <66876303+mjakulica@users.noreply.github.com>
2026-03-28 20:20:42 -0300

    Create tailwind.config.ts file

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+// tailwind.config.ts
+
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
